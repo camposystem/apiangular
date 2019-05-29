5787b496d50825adb331d7b087fc248a09307c83
camposystem <camposystem@gmail.com>
2019-05-28 23:15:36 -0300

    28052019

---
+++ Dominios/Controllers/DominiosController.cs
@@ -0,0 +1,29 @@
+﻿using Dominios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dominios.Controllers
+{
+    public class DominiosController : ApiController
+    {
+        private static List<Dominio> dominios = new List<Dominio>();
+ 
+        [HttpGet]
+        public string ObterDominio()
+        {
+            string arraydominio = "[ {\"id\": \"1\", \"nome\":\"Criado\"},{ \"id\": \"21\",\" nome\": \"Alterado\"},{\"id\": \"41\", \"nome\":\"Transporte\"}]";
+
+            return arraydominio.ToString();
+        }
+
+        [HttpPost]
+        public void Post(int id, string nome)
+        {
+            dominios.Add(new Dominio(id, nome));
+        }
+    }
+}
---
+++ Dominios/Models/Dominio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dominios.Models
+{
+    public class Dominio
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+
+        public Dominio(int id, string nome)
+        {
+            this.Id = id;
+            this.Nome = nome;
+        }
+    }
+}
